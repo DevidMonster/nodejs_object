1848fd3ecbef70975f56bb764af74b702072d839
DevidMonster <mrbat905@gmail.com>
2022-12-21 16:49:34 +0700

    template engine

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const { extname } = require("path");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+
+app.listen(port, () => {console.log(`Bạn đang chạy ở cổng: http://localhost:${port}`)})
