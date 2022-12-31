0cba6442224fc7613ab24720654da2ba6c7822c1
DevidMonster <mrbat905@gmail.com>
2022-12-31 23:46:17 +0700

    design template and mvc

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
-const { extname } = require("path");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const { extname } = require('path');
+const exp = require('constants');
 const app = express();
-require("dotenv").config();
+require('dotenv').config();
 const port = process.env.PORT || 3000;
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', engine({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources/views'));
 
+route(app);
 
-
-app.get("/", (req, res) => {
-    res.render('home')
-})
-
-app.listen(port, () => {console.log(`Bạn đang chạy ở cổng: http://localhost:${port}`)})
+app.listen(port, () => {
+    console.log(`Bạn đang chạy ở cổng: http://localhost:${port}`);
+});
